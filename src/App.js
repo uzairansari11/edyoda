bec0f7911a5a0a9addea60ea74cc7491248cd077
Uzair Ansari <uzairans532@gmail.com>
2023-07-15 10:59:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/payment page/Payment";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/payment page/Payment.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Payment.module.css"
+const HomePage = () => {
+  return (
+    <div className={styles.container}>
+    </div>
+  );
+};
+
+export default HomePage;
